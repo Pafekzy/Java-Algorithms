b62e7c7f6be95021506ad13bd1077da084d28292
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-03-26 12:32:11 +0100

    test failed again due to inconsistent matrix setting

--- NumberSpiral.java
+++ NumberSpiral.java
@@ -2,6 +2,23 @@ package DeitelChapter2Algorithm;
 
 public class NumberSpiral {
     public static int[][] generateSpiral(int size) {
-        return new int[size][size];
+        int[][] spiral = new int[size][size];
+        int value = 1;
+        int left = 0, right = size - 1, top = 0, bottom = size - 1;
+
+        while (value <= size * size) {
+            for (int i = left; i <= right; i++) spiral[top][i] = value++;
+            top++;
+
+            for (int i = top; i <= bottom; i++) spiral[i][right] = value++;
+            right--;
+
+            for (int i = right; i >= left; i--) spiral[bottom][i] = value++;
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) spiral[i][left] = value++;
+
+        }
+        return spiral;
     }
 }
--- NumberSpiralTest.java
+++ NumberSpiralTest.java
@@ -8,8 +8,8 @@ public class NumberSpiralTest {
     void testSpiralMatrixOfSizeThree() {
         int[][] expected = {
                 {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {8, 9, 4},
+                {7, 6, 5}
         };
         assertArrayEquals(expected, NumberSpiral.generateSpiral(3));
     }
