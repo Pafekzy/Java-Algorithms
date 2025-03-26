eaf7fde29566a067d40e10e8bd02dc22c69eba3a
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-03-26 12:50:36 +0100

    Test case for a Spiral matrix of size of 4

--- NumberSpiralTest.java
+++ NumberSpiralTest.java
@@ -30,6 +30,16 @@ public class NumberSpiralTest {
         assertArrayEquals(expected, NumberSpiral.generateSpiral(3));
     }
 
+    @Test
+    void testSpiralMatrixOfSizeFour() {
+        int[][] expected = {
+                { 1,  2,  3,  4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10,  9,  8, 7}
+        };
+        assertArrayEquals(expected, NumberSpiral.generateSpiral(4));
+    }
 
 
 }
