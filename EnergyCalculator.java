800b761063657d0d450c1aa7128579ffa57a38f9
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-04-04 04:16:53 +0100

    Initialize project structure with main class EnergyCalculator.

---
+++ EnergyCalculator.java
@@ -0,0 +1,13 @@
+package DeitelChapter2Algorithm;
+
+import java.util.Scanner; // Import Scanner for user input
+
+public class EnergyCalculator {
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in); // Create a Scanner object for input
+
+
+
+    }
+}
