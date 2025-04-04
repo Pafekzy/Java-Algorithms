71b4513658d9b01daf4e0b4c1a8ff1eb28178dcc
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-04-04 04:20:32 +0100

    Add Basic User Input Functionality

--- EnergyCalculator.java
+++ EnergyCalculator.java
@@ -7,6 +7,10 @@ public class EnergyCalculator {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in); // Create a Scanner object for input
 
+        // Prompt the user to enter the amount of water in kilograms
+        System.out.print("Enter the amount of water in kilograms: ");
+        double waterMassKilograms = inputScanner.nextDouble(); // Read the mass of water
+
 
 
     }
